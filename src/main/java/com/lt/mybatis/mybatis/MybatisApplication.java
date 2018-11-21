41af6af72e95d707f9a87244c405e6604e73ac98
Evolution <782998342@qq.com>
2018-11-21 15:36:07 +0800

    上传源码

---
+++ src/main/java/com/lt/mybatis/mybatis/MybatisApplication.java
@@ -0,0 +1,11 @@
+package com.lt.mybatis.mybatis;
+
+
+
+@SpringBootApplication
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
